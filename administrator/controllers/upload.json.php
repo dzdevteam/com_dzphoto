da7ca0f8f1e0d6b5379e4d836282fafee815a358
Lê Hoàng Phương <herophuong93@gmail.com>
2013-10-28 11:57:52 +0700

    fix duplicating-alias generation

--- administrator/controllers/upload.json.php
+++ administrator/controllers/upload.json.php
@@ -72,7 +72,7 @@ class DZPhotoControllerUpload extends JControllerLegacy
                     array(
                         'id' => 0, 
                         'title' => pathinfo($links['original'], PATHINFO_BASENAME),
-                        'alias' => pathinfo($file['name'], PATHINFO_FILENAME),
+                        'alias' => pathinfo($links['original'], PATHINFO_FILENAME),
                         'links' => $links,
                         'album' => $album
                     )
